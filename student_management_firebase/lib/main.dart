ee9fc07f099ee78bfe8c50e22874feb945781095
Rastra17 <57227732+Rastra17@users.noreply.github.com>
2022-11-17 12:30:41 +0545

    ggz

--- student_management_firebase/lib/main.dart
+++ student_management_firebase/lib/main.dart
@@ -2,19 +2,40 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("App"),
-          backgroundColor: const Color.fromARGB(255, 43, 209, 173),
-          elevation: 0,
-        ),
-        body: const Text(
-          'Something',
-          style: TextStyle(fontSize: 16),
+    const MaterialApp(debugShowCheckedModeBanner: false, home: MyWidget()),
+  );
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("App"),
+        backgroundColor: Colors.black,
+        elevation: 0,
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(8.0),
+        child: TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.red),
+              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.green),
+              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+            ),
+            labelText: "Enter Name",
+          ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
