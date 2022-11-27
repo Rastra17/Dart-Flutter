97add99a1c5f73bbefce235c1f46b9a6f719f670
Rastra17 <57227732+Rastra17@users.noreply.github.com>
2022-11-27 20:33:33 +0545

    self problem to solve discord video upload

---
+++ Basics/FileSizeProblem.dart
@@ -0,0 +1,45 @@
+import "dart:io";
+
+class CalculatePercentageInterface {
+  double Percentage() {
+    print("This method is for calculating Percentage.");
+    return 0.0;
+  }
+
+  void setValues(double percentage) {
+    print("This method is to assign values within instances.");
+  }
+}
+
+class ProblemPercentage implements CalculatePercentageInterface {
+  double _filesize = 0.0;
+  int _neededsize = 8192;
+  double _percentage = 0.0;
+
+  ProblemPercentage() {
+    print("Percentage Calculator");
+  }
+
+  @override
+  void setValues(double filesize) {
+    this._neededsize = _neededsize;
+    this._filesize = filesize;
+    this._percentage = _percentage;
+  }
+
+  @override
+  double Percentage() {
+    this._percentage = (this._neededsize / this._filesize) * 100;
+    return this._percentage;
+  }
+}
+
+void main(List<String> args) {
+  ProblemPercentage object = ProblemPercentage();
+
+  stdout.write("Enter the file size: ");
+  double filesize = double.parse(stdin.readLineSync()!);
+
+  object.setValues(filesize);
+  print("The Percentage needed to optimize file is: ${object.Percentage()}%");
+}
